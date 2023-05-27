78b5c35e03cf3d26f340c504e33f46c4c942ce67
carietti <134340979+carietti@users.noreply.github.com>
2023-05-26 23:08:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,18 +21,18 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+ def get_fruityvice_data(this_fruit_choice): 
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      #streamlit.text(fruityvice_response.json())
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 try:
     #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi') -- preloaded with Kiwi
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice: streamlit.error('Please select a fruit to get information')
     else: 
-      streamlit.header("Fruityvice Fruit Advice!")
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-      #streamlit.text(fruityvice_response.json())
-
-      # write your own comment -what does the next line do? 
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      # write your own comment - what does this do?
+      fruit_data = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
       streamlit.error()
